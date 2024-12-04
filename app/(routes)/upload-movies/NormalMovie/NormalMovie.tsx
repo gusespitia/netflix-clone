3d44d1ad954b6bd316594df2c755a53636617773
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2024-12-04 14:25:01 +0100

    Update NormalMovie.tsx

--- app/(routes)/upload-movies/NormalMovie/NormalMovie.tsx
+++ app/(routes)/upload-movies/NormalMovie/NormalMovie.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Upload } from "lucide-react";
 import { useState } from "react";
 import axios from "axios";
-import { dataMovies } from "@/data/data-movies/NormalMovies.data";
+import { dataMovies } from "@/data/data-movies/NormalMovie.data";
 import { toast } from "@/hooks/use-toast";
 
 const NormalMovie = () => {
