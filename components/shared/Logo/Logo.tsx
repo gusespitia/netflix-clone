993941898bfc0b9a2584ff23ab187813f4015381
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2024-12-11 14:40:30 +0100

    Update Logo.tsx

--- components/shared/Logo/Logo.tsx
+++ components/shared/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo = () => {
     return ( 
         <Link href="/" className="text-[#E50914] font-extrabold text-2xl">
-        GusDev
+        GUSFLIX
         </Link>
   );
 }
