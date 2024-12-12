40ffdbe2e431724cee4fbec0d93449f62d115abd
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2024-12-12 12:12:53 +0100

    login image full

--- app/(routes)/(auth)/layout.tsx
+++ app/(routes)/(auth)/layout.tsx
@@ -1,22 +1,27 @@
 import Logo from "@/components/shared/Logo/Logo";
 
-export default function AuthLayouth({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-full">
-      <div className="relative h-full">
-        <div className="-z-10 absolute bg-black w-full h-full min-h-screen">
-          <div className="bg-[url('/login-bg.jpg')] bg-cover bg-no-repeat opacity-40 h-full" />
+    <div className="relative w-full min-h-screen">
+      {/* Background */}
+      <div className="-z-10 absolute inset-0">
+        <div className="bg-black w-full h-full">
+          <div
+            className="bg-[url('/login-bg.jpg')] bg-cover bg-no-repeat bg-center opacity-40 w-full h-full"
+          />
         </div>
-        <div className="mx-auto px-8 py-5 max-w-7xl">
-          <Logo />
-        </div>
-        <div className="mx-auto w-full max-w-md h-full">
-          <div className="bg-black px-14 py-6 rounded-lg">{children }</div>
-        </div>
+      </div>
+
+      {/* Content */}
+      <div className="relative mx-auto px-8 py-5 max-w-7xl">
+        <Logo />
+      </div>
+      <div className="relative flex justify-center items-center mx-auto w-full max-w-md min-h-screen">
+        <div className="bg-black px-14 py-6 rounded-lg">{children}</div>
       </div>
     </div>
   );
